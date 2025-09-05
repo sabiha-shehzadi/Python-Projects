bc25ccd2799f4b7ab6b372fca51a0bf6bc2a7fb6
Sabiha Shehzadi <109547176+sabiha-shehzadi@users.noreply.github.com>
2025-09-05 10:15:42 +0500

    Refactor DiceRoll.py for improved usability

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,14 +1,14 @@
-import random
+import random #import the random library to use randint
 
-while True:
+while True: # as we dont know about the iterations
     choice = input("Enter your choice to get a roll (y/n): ")
-    if choice.lower() == "y":
-        dice1 = random.randint(1, 6)
+    if choice.lower() == "y": # for better useability
+        dice1 = random.randint(1, 6) # starting and ending parameters 
         dice2 = random.randint(1, 6)
-        print({dice1, dice2})
+        print([dice1, dice2]) # use tuple or list instead of sets
     elif choice.lower() == "n":
         print("no roll was requested!")
-        break
+        break # will stop while loop 
     else:
         print("invalid choice!")
         print("Please enter 'y' or 'n'.")
